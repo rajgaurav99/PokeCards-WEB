ec1bb5bf0629471e7986db67e0535b8bf1e268b1
Gaurav Kumar Singh <rajgaurav99@gmail.com>
2020-03-05 13:17:24 +0530

    Bug fixes and Improvements

--- js/pokemon_service.js
+++ js/pokemon_service.js
@@ -11,7 +11,7 @@
         return $http(
           {
             method:"GET",
-            url:"../data/pokemon_full.json"
+            url:"https://rajgaurav99.github.io/PokeCards/data/pokemon_full.json"
           }
         )
         .then(function (result) {
